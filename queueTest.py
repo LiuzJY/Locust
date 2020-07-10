2ca251445fa16328eb6ff258577b5781a3547a37
LiuzJY <48585263+LiuzJY@users.noreply.github.com>
2020-07-10 17:41:34 +0800

    Update queueTest.py

--- queueTest.py
+++ queueTest.py
@@ -1,26 +1,24 @@
+# -*- coding:utf-8 -*-
+import io
 from locust import HttpLocust, TaskSet, task
-import queue
+try:
+    import queue
+except ImportError:
+    import Queue as queue
 
 
 class MyTaskSet(TaskSet):
-    # headers = {"channel": "014000D",
-    #            "version": "5.0.1",
-    #            "ua": "123",
-    #            "msisdn": "123",
-    #            "uid": "123",
-    #            "logId": "123",
-    #            "IMEI": "123",
-    #            "idfa": "123"}
+
     def on_start(self):
         global data
         try:
             data = self.locust.queueData.get_nowait()
-            print("++++++++++++++++++++++++++++++++++++++++++++++++++")
-            print("获取队列data:%s" % data)
+            # print("++++++++++++++++++++++++++++++++++++++++++++++++++")
+            # print("获取队列data:%s" % data)
         except queue.Empty:
             print("no data")
             exit(0)
-        print('actually msisdn and uid is {} and {}'.format(data['msisdn'], data['uid']))
+        # print('actually msisdn and uid is {} and {}'.format(data['msisdn'], data['uid']))
         self.locust.queueData.put(data)
         headers = {
             "channel": "123",
@@ -34,39 +32,47 @@ class MyTaskSet(TaskSet):
         }
         return headers
 
-    @task(1)
-    # 查询我的排名
-    def queryMyRankByAct(self):
-        url = "/URL/123/123/123"
+    # @task(15)
+    # 查询福袋状态接口
+    def queryMiguCoinOpenState(self):
+        url = "/url/123/123/123"
         params = {"activityId": "123",
-                  "singerId": "123",
-                  "stage": "123"}
-        headers = MyTaskSet.on_start(self)
-        with self.client.get(url, headers=headers, params=params, catch_response=True)as response:
-                        if int(response.elapsed.total_seconds()) > 2:
-                response.failure("response timeout 2s")
-            if response.text.find("\"code\":\"000000\"") < 1:
-                response.failure("code is not 000000")
+                  "activityStage": "123"}
+        with self.client.get(url, headers=MyTaskSet.on_start(self), params=params, catch_response=True)as response:
+            # print(response.text)
+            if float(response.elapsed.total_seconds()) > 1:
+                # print(response.text)
+                response.failure("response timeout 1s：" + str(response.elapsed.total_seconds()))
+            if response.text.find("\"code\":\"199999\"") > 0:
+                # print(response.text)
+                response.failure("code is 199999")
             else:
                 response.success()
 
 
+
+
 class MyLocust(HttpLocust):
     task_set = MyTaskSet
+    min_wait = 1
+    max_wait = 3
+    host = "http://www.123.com"
     queueData = queue.Queue(maxsize=100)
-    f = open("123.txt")
-    lines = f.readlines()
-    for line in lines:
-        msisdn = line.split(",")[0]
-        uid = line.rstrip('\n').split(",")[1]
-        data = {
-            "msisdn": msisdn,
-            "uid": uid
-        }
-        print(data)
-        queueData.put(data)
+    with io.open("123.txt", encoding="utf-8") as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            msisdn = line.split(",")[0]
+            uid = line.rstrip('\n').split(",")[1]
+            data = {
+                "msisdn": msisdn,
+                "uid": uid
+            }
+            queueData.put(data)
 
 
 if __name__ == "__main__":
     import os
     os.system("locust -f queueTest.py --host=http://www.123.com")
+
